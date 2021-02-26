78570253019fa0299afac7d5b4a09b89154ae75c
ahmadrobit <ahmadrobit@yahoo.co.id>
2021-02-26 13:29:00 +0700

    ui blade

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('home', function(){
+    return view('home');
+});
+Route::get('hello', function(){
+    return view('hello');
+});
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
